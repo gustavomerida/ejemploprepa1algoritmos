9c1cfb0e09d63eb2896c81bce330c8bc24973ea8
Gustavo Mérida <gmerida@correo.unimet.edu.ve>
2025-01-10 20:16:03 -0400

    ejercicio del final de la prepa 1

--- prepa1semana1.py
+++ prepa1semana1.py
@@ -1,2 +1,14 @@
-print("Hello World!")
-print("Hello Again")
+#Programa que solicite nombre y edad y devuelva un saludo y el año en que naciste
+año = 2025
+nombre = input("¿Cuál es tu nombre? -->")
+edad = input("¿Cuántos años tienes? -->")
+# Validar que la entrada sea un número
+edad = int(edad) #NO sabemos si el usuario puso algo que no sea puede transformar
+
+# Calcular el año de nacimiento
+año_nacimiento = año - edad
+
+print(f"Hola {nombre} naciste en el año {año_nacimiento}")
+
+
+
